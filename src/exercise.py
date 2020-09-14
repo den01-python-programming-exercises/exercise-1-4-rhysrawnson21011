e1a23e836d5a105cb767bdb8bd6230c84aca801a
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-09-14 08:44:32 +0000

    print message

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,8 @@
 def main():
-    #write your code below this line
-    message = input("Write a message...")
-
+  
+   message = input("loser")
+   print (message)
+  
+  
 if __name__ == '__main__':
     main()
